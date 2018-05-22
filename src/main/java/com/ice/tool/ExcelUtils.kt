dcb4026fbd0fd86c7df9a399b07f27751f162d87
ice <1034939359@qq.com>
2018-05-22 11:48:55 +0800

    first commit

--- src/main/java/com/ice/tool/ExcelUtils.kt
+++ src/main/java/com/ice/tool/ExcelUtils.kt
@@ -2,7 +2,8 @@ package com.ice.tool
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.CellType
-import java.io.InputStream
+import java.io.*
+import java.util.LinkedHashMap
 
 object ExcelUtils {
 
@@ -29,5 +30,58 @@ object ExcelUtils {
             }
         }
         return list
+    }
+
+    @Suppress("unused")
+            /**
+     *
+     * @param filePath 生成的excel文件路径和名称
+     * @param maps 数据最好不要为null或者size为0,map的键值为excel的第一行标题
+     * @return 成功或失败
+     */
+    fun toExcel(filePath: String, maps: List<LinkedHashMap<String, String>>?): Boolean {
+        @Suppress("SENSELESS_COMPARISON")
+        if (filePath==null|| "" == filePath) {
+            return false
+        }
+        if (maps == null) {
+            return false
+        }
+        if (maps.isEmpty()) {
+            return false
+        }
+        val workbook = HSSFWorkbook()
+        val sheet = workbook.createSheet("Sheet1")
+        val rowTitle = sheet.createRow(0)
+        val mapTitle = maps[0]
+        for ((cellTitleCount, s) in mapTitle.keys.withIndex()) {
+            val cell = rowTitle.createCell(cellTitleCount)
+            cell.setCellValue(s)
+        }
+        var rowNum = 1
+        for (map in maps) {
+            val row = sheet.createRow(rowNum++)
+            for ((colNum, s) in map.values.withIndex()) {
+                val cell = row.createCell(colNum)
+                cell.setCellValue(s)
+            }
+        }
+        try {
+            val file = File(filePath)
+            if (file.exists()) {
+                file.delete()
+            }
+            file.parentFile.mkdirs()
+            file.createNewFile()
+            val outputStream = FileOutputStream(file)
+            workbook.write(outputStream)
+            workbook.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return true
     }
 }
