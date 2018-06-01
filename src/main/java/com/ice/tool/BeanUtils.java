ae7afbe1873863a3e0196b3690e585f570bf7f86
ice <1034939359@qq.com>
2018-06-01 11:36:16 +0800

    first commit

---
+++ src/main/java/com/ice/tool/BeanUtils.java
@@ -0,0 +1,26 @@
+package com.ice.tool;
+
+import java.lang.reflect.Field;
+
+public class BeanUtils {
+
+    /**
+     * 复制新对象中有参数的字段到原有对象中
+     * @param object1 修改的对象
+     * @param object2 参考的对象
+     * @param <T> 泛型
+     */
+    public static <T> void copy(T object1,T object2) throws IllegalAccessException, InstantiationException {
+        Class classObj=object1.getClass();
+        Object obj=classObj.newInstance();
+        Field[] fields=classObj.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);//暴力反射，解除私有限定
+            Object defaultStr=field.get(obj);
+            Object object2Str=field.get(object2);
+            if(!defaultStr.equals(object2Str)){
+                field.set(object1,object2Str);
+            }
+        }
+    }
+}
